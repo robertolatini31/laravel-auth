5a1dcb79cf9e53897b311a9a6759a33c85824cb2
robertolatini31 <robertolatini31@gmail.com>
2022-06-21 16:49:07 +0200

    add vue

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
@@ -28,7 +26,7 @@ Route::middleware('auth')->namespace('Admin')->name('admin.')->prefix('admin')->
 
 
 Route::get("{any?}", function(){
-    return view("guest.home");
+    return view("guests.home");
 })->where("any", ".*");
 
 
